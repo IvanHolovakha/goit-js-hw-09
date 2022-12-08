3c07ca139ebcd80f4981f7fb47e3b963ebd9076a
IvanHolovakha <meisuiwanreiser@gmail.com>
2022-12-08 21:01:38 +0200

    add 1,js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,14 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+ console.log(1)
 
+ startBtn.addEventListener("click", onStartBtnClick);
+//  stopBtn.addEventListener("click", onStopBtnClick);
+
+ function onStartBtnClick(event){
+    setInterval(
+        function getRandomHexColor() {
+            return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+          }, 1000
+    )
+ }
