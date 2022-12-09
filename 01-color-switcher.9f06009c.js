2da19af3bc1d02f20e8b5b83341d440fef7128fb
IvanHolovakha <98047813+IvanHolovakha@users.noreply.github.com>
2022-12-09 11:02:45 +0000

    Deploying to gh-pages from @ IvanHolovakha/goit-js-hw-09@8a00729788103c98a3b114c8006a37f4d55c11ac 🚀

--- 01-color-switcher.9f06009c.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]");document.querySelector("button[data-stop]");console.log(1),t.addEventListener("click",(function(t){setInterval((function(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)}))}();
-//# sourceMappingURL=01-color-switcher.9f06009c.js.map
--- 01-color-switcher.f8ccb69c.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]");document.querySelector("button[data-stop]");console.log(1),t.addEventListener("click",(function(t){setInterval((function(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)}));
-//# sourceMappingURL=01-color-switcher.f8ccb69c.js.map
