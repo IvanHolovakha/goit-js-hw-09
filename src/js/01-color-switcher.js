8a00729788103c98a3b114c8006a37f4d55c11ac
IvanHolovakha <meisuiwanreiser@gmail.com>
2022-12-09 13:02:19 +0200

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,14 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
- console.log(1)
 
- startBtn.addEventListener("click", onStartBtnClick);
-//  stopBtn.addEventListener("click", onStopBtnClick);
+let timerId = null;
 
- function onStartBtnClick(event){
-    setInterval(
-        function getRandomHexColor() {
-            return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-          }, 1000
-    )
- }
+startBtn.addEventListener("click", () => {
+  timerId = setInterval( ()=>{
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+});
